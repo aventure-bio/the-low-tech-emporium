5e8e858d6db0584e21b476183c1384d8b02f9041
Damien Le Thiec <damien.lethiec@gmail.com>
2019-12-17 16:31:41 +0100

    first draft pwa

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+To build: `JEKYLL_ENV="production" jekyll build`
---
+++ _site/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "_site/",
+  "globPatterns": [
+    "**/*.{html,css,js,json,md,txt,xml}"
+  ],
+  "swDest": "sw.js"
+};
